3f39ae79a25fad9f12ebf713141f4cb3d18a213e
hvivanconunez <hvivanconunez@gmail.com>
2018-01-16 19:02:13 -0500

    Ejecución Inicial
    Iniciación del desarrollo del Proyecto.
    
    Hora Inicio: 15:00
    Hora Final: 19:02

---
+++ includes/footer.php
@@ -0,0 +1,104 @@
+<footer id="contact" class="footer action-lage bg-black p-top-80">
+                <!--<div class="action-lage"></div>-->
+                <div class="container">
+                    <div class="row">
+                        <div class="widget_area">
+                            <div class="col-md-3">
+                                <div class="widget_item widget_about">
+                                    <h5 class="text-white">Nosotros</h5>
+                                    <p class="m-top-20">Somos un grupo de Profesionales que nos dedicamos a Asesorar Tesis en Derecho y Aministración.</p>
+                                    <div class="widget_ab_item m-top-30">
+                                        <div class="item_icon"><i class="fa fa-location-arrow"></i></div>
+                                        <div class="widget_ab_item_text">
+                                            <h6 class="text-white">Ubicación :</h6>
+                                            <p>Jr. Tarapaca 159</p>
+                                        </div>
+                                    </div>
+                                    <div class="widget_ab_item m-top-30">
+                                        <div class="item_icon"><i class="fa fa-phone"></i></div>
+                                        <div class="widget_ab_item_text">
+                                            <h6 class="text-white">Celular :</h6>
+                                            <p>+51 987 547 741</p>
+                                        </div>
+                                    </div>
+                                    <div class="widget_ab_item m-top-30">
+                                        <div class="item_icon"><i class="fa fa-envelope-o"></i></div>
+                                        <div class="widget_ab_item_text">
+                                            <h6 class="text-white">E-mail :</h6>
+                                            <p>vivanco_n@hotmail.com</p>
+                                        </div>
+                                    </div>
+                                </div><!-- End off widget item -->
+                            </div><!-- End off col-md-3 -->
+
+                            <div class="col-md-3">
+                                <div class="widget_item widget_about">
+                                    <h5 class="text-white">Horarios de Atención</h5>
+                                    <div class="widget_ab_item m-top-30">
+                                        <div class="item_icon"><i class="fa fa-clock-o"></i></div>
+                                        <div class="widget_ab_item_text">
+                                            <h6 class="text-white">Lunes a Viernes :</h6>
+                                            <ul>
+                                                <li>Mañana: 9:00 - 13:00</li>
+                                                <li>Tarde: 16:00 - 19:30</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="widget_ab_item m-top-30">
+                                        <div class="item_icon"><i class="fa fa-clock-o"></i></div>
+                                        <div class="widget_ab_item_text">
+                                            <h6 class="text-white">Sábados :</h6>
+                                            <ul>
+                                                <li>Mañana: 9:00 - 13:00</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div><!-- End off widget item -->
+                            </div><!-- End off col-md-3 -->
+
+                            <div class="col-md-3">
+                                <div class="widget_item widget_service sm-m-top-50">
+                                    <h5 class="text-white">Páginas de Interes</h5>
+                                    <ul class="m-top-20">
+                                        <li class="m-top-20"><a href=""><i class="fa fa-angle-right"></i> Página 1</a></li>
+                                        <li class="m-top-20"><a href=""><i class="fa fa-angle-right"></i> Página 2</a></li>
+                                        <li class="m-top-20"><a href=""><i class="fa fa-angle-right"></i> Página 3</a></li>
+                                        <li class="m-top-20"><a href=""><i class="fa fa-angle-right"></i> Página 4</a></li>
+                                        <li class="m-top-20"><a href=""><i class="fa fa-angle-right"></i> Página 5</a></li>
+                                        <li class="m-top-20"><a href=""><i class="fa fa-angle-right"></i> Página 6</a></li>
+                                    </ul>
+                                </div><!-- End off widget item -->
+                            </div><!-- End off col-md-3 -->
+
+                            <div class="col-md-3">
+                                <div class="widget_item widget_newsletter sm-m-top-50">
+                                    <div class="widget_brand">
+                                        <a href="" class="text-uppercase">Thesis In Iure</a>
+                                        <!--
+                                        <p>Lorem ipsum dolor sit amet consec tetur 
+                                            adipiscing elit nulla aliquet pretium nisi in</p>
+                                        -->
+                                    </div>
+                                    <ul class="list-inline m-top-20">
+                                        <li>-  <a href=""><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href=""><i class="fa fa-youtube"></i></a> -</li>
+                                    </ul>
+
+                                </div><!-- End off widget item -->
+                            </div><!-- End off col-md-3 -->
+                        </div>
+                    </div>
+                </div>
+                <div class="main_footer fix bg-mega text-center p-top-40 p-bottom-30 m-top-80">
+                    <div class="col-md-12">
+                        <p class="wow fadeInRight" data-wow-duration="1s">
+                            Hecho
+                            por 
+                            Héctor Vivanco
+                            . Todos los Derechos Reservados
+                        </p>
+                    </div>
+                </div>
+            </footer>
